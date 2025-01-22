24c6f4276f077813aedb82590f70ddfa90dd2eb6
Chottcodes <cwellshott@gmail.com>
2025-01-22 09:20:39 -0800

    C# quiz

---
+++ Controllers/AddTwoNumbers.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C__Quiz.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AddTwoNumbers : ControllerBase
+    {
+        
+    }
+}
